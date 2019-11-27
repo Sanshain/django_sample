0eab1ab35a77b5df07087a0fd10aee9d18f0d722
Sanshain <digital-mag@ya.ru>
2019-11-27 19:36:18 +0300

    check error for sample

--- main/static/js/snippet.js
+++ main/static/js/snippet.js
@@ -130,7 +130,7 @@ function fragment_refresh(e){
 					_boxes =_box.parentElement.querySelectorAll('[id]');
 				}
 				else{
-					var sample = signs[0];
+					var sample= _box.getElementById(signs[0]);
 					
 					//если типовой элемент найден
 					if (sample){
