47bab51f07b7465e754b5a42aa77ad33577bfa59
Sanshain <digital-mag@ya.ru>
2019-12-09 22:27:27 +0300

    RefreshManager.Initialize

--- main/static/js/snippet_orig.js
+++ main/static/js/snippet_orig.js
@@ -1,366 +1,13 @@
 
-/*!
-
-	e - expected event object
-*/
-RefreshManager.Initialize = function(e){	
-
-	//если это ссылка, берем из адреса
-	//если это кнопка, берем из formAction-атрибута
-	
-	var target = 
-		e.target.href.substr(location.origin.length)
-		|| e.target.formAction;						
-	//если иное, берем из атрибута data-to
-	target =target||e.target.getAttribute('data-to');
-
-	
-	if (!target) {
-		
-		throw new Error('RefreshManager cant be initialized');
-	};
-		
-	if (!window.atob){					//just for <ie10
-
-		document.location.href = target;			
-	}
-	else {
-		e.preventDefault();	
-		
-		var _rm = new RefreshManager(e);
-		
-			   _rm.target = target;
-		return _rm;			//если нет, кастомизируем
-	};
-}
 
 
+function fragment_refresh(e){
 
-function RefreshManager(e, root_elem){
-	
-	var root_elem=root_elem || 'content';   	//obsolete
-	
-	var unique_templates = 
-		e.target.dataset['_refresh']
-			.replace(/[\s]+/,'')
-			.split(',');	
-	
-	this.aim_blocks = [];			// id блоков для запроса 
-	
-	
-	var get_boxes = function(block_name){
-		
-		
-		var details = block_name.split(">");
-		
-		var _box = document.getElementById(details[0]);
-		
-		if (!_box)
-			throw new Error('root element not found');
-	
-		var _boxes = []; 		// боксы для анимации		
-		
-		if (details[1]){
-		
-			var signs = details[1].split('.');
-			
-			var sign = signs.indexOf('*');
-			
-			if (sign>=0){
-				if (sign==0){
-					//если не заданы одноуровневые поля
-					
-					//такого случая пока нет, но скорее всего брать все дочерние с id 
-					
-					//(либо data-state)
-				}			
-				else{
-					var sample = _box.querySelector(
-						'#'+signs[0]
-					);	
-					//если типовой элемент найден
-					if (sample){
-						_boxes=_box.querySelectorAll(
-							'[id]'
-						);
-						this.aim_blocks.push('*'+_box.id);
-						//применяем content_waiting к каждому элементу
-					}
-					else{
-						//значит надо обновить корневой элемент: ничего не делаем
-					}					
-				}
-			}
-			else{
-				//если нет обощителя, значит ищем каждый указанный элемент
-				for(var key in signs)
-				{
-					var line = details[1].querySelector(
-						'#'+elems[key]
-					);
-					_boxes.push(line);
-					this.aim_blocks.push(elems[key]);
-				}								
-			}		
-		}
-		
-		return _boxes.length ? _boxes : _box;
-	}
-	
-						
-	function _animate(elem, visible){
-		if (!visible){ // скрываем
-		
-			//можно засунуть в класс
-			elem.style.transition='0.5s ease-out';
-			elem.style.opacity = 0;			
-			elem.style.transform = 'scale(0.9,0.9)';								
-			var _content = search_fixed(elem);
-
-			if (!_content) return;
-			else 
-				_content.style.top = '0';
-			
-		}
-		else{
-		
-			var temp_top = null;
-		
-			//тут написать спец ф-ю, которая ищет элементы с fixed до первого дерева с дочерними элементами больше 1			
-			
-			var _content = null;
-			
-			if (elem.id == 'main' || elem.id == 'content'){
-				//var _content = elem.children[0];
-				var _content = search_fixed(elem);
-				
-				if (_content){
-//!
-					temp_top = window.getComputedStyle(_content).top;		//либо 
-					//temp_top = _content.offsetTop;
-					_content.style.top = '0';//*/
-				}
-				
-			}
-
-			//показывает информацию через 1 сек
-			setTimeout(function(){
-				//возврат в top после анимации, чтобы не скроллился
-				elem.style.transition = 'none';			
-				elem.style.transform = 'none';
-								
-				if (temp_top) _content.style.top = temp_top;
-				
-				setTimeout(function(){
-					elem.style.transition = '0.5s';
-				}, 20);
-				
-			},1000);								
-
-			elem.style.opacity =1;
-//!
-			if (!_content) return; //<-скорее всего это убрать
-			
-			elem.style.transform='scale(1,1)';			
-
-		}
-	}							
-	
-	
-	/*!  регулярует вызов функции анимации...
-	
-		\brief регулярует вызов функции анимации для каждого..
-		блока и подблока после получения данных
-		
-		@param deep - глубина ожидания
-		@param box - блок, для которого ожидается анимация
-		
-	*/
-	function _await__animate(deep, box){
-	
-		console.log(deep + ' - waiting for ' + box.id);
-		
-		
-		if (!deep){ //animation отсутствия интернета
-			
-			alert('нет соединения с сервером');
-			
-			return false;
-		}										
-	
-		setTimeout(function(){
-			
-			if (responsed_content){
-
-				render_page(
-					responsed_content.pop(), 
-					responsed_content.pop()
-				);	
-				
-				//происходит анимация
-				setTimeout(function(){ //box.style.opacity =1;
-					
-					
-					//box.className = 'block';
-					_animate(box, true);
-				}, 40);
-			}
-			else {
-				
-				_await__animate(--deep, box);
-			}			
-
-		},700);								
-	}	
-	
-	
-	var animate_handle = function(block_name){
-		
-		var responsed_content = null;
-		
-		var _boxes = get_boxes(block_name);
-		
-		if (_boxes.length){
-			
-			for (var k=0;k<_boxes.length;k++){
-				
-				_animate(_boxes[k], false);
-				
-				_await__animate(3, _boxes[k]);
-			}
-		}
-		else{
-			_animate(_boxes, false);
-			
-			_await__animate(3, _boxes);
-						
-			aim_blocks.push(_boxes.id);			
-		}
-		
-	}
-	
-	for (var key in unique_templates){
-		
-		animate_handle(unique_templates[key]);
-	}
-		
-	var nec_blocks = requested_blocks_by_require();
-
-	//var blocks = aim_blocks.concat(nec_blocks);
-	
-	
-	
-	this.Commit = function(){
-		var box_onload = function (resp, set_url)
-		{
-			responsed_content = [set_url,resp];			
-		}		
-			
-		var ajax = new Ajax(this.target, box_onload);
-		ajax.onfail = function(){
-			//здесь может быть относительно 
-			// навязчивое сообщение о том, что 
-			//ваш браузер не поддерживает 
-			//автоматические переходы
-			alert('ваш браузер не смог осуществить частичное обновление контента страницы.'+
-			' Нажмите ок, чтобы перейти напрямую');
-			document.location.href = this.target;
-		};
-		
-		var args = target.match(/\d+/g);//аргументы
-		
-		var q = [args, nec_blocks, aim_blocks];
-		
-		ajax.submit_json(q);		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	function requested_blocks_by_require(){	
-	
-		var requared_blocks = 
-			e.target.dataset['_require'].split('.');
-			
-		var r_blocks = [];
-		
-		
-		for(var key in requared_blocks){
-			
-			var detail = requared_blocks[key].split('|');
-			
-			var b_id = detail.pop(); //id элемента
-			var r_state =detail.length ?detail.pop(): '';
-			var state = '';
-			
-			required_block = dom.obj(b_id);
-			
-			if (required_block && r_state){
-				//получили блок. Теперь необходимо получить его состояние:
-				
-				//для (неуправляющего) простого тега (который содержит только текст) допускается атрибут data-state, который содержит состояние этого элемента (например data-state='1')
-				state = 	
-					required_block.getAttribute['data-state'];
-				
-				//если тег является управляющим (управляющие элементы не содержат своего состояния), то ловим состояние в его контенте, а именно - в элементе, управляющем позиционированием контента, - это первый элемент контента
-				if (!state){
-					var elem=
-						required_block.children[0];
-						
-					//его состояние хранится в его id
-					//либо в data-state, либо и там и там
-					state = 
-						elem? 
-							elem.id+
-							elem.dataset['state'] || ""
-						:"";
-						
-				}
-				
-			}
-			
-			//теперь у нас есть required_block и r_state:
-			//для выполнения условия required_block должен быть thruthy, а state==r_state
-			
-			if (required_block && r_state==state){
-				continue;
-			}
-			else 
-				r_blocks.push(b_id);
-		
-		
-		}
-		
-		return r_blocks;
-		
-	}
-	
-}
-
-
-
-
-
-
-
-
-
-function fragment_refresh1(e){
-	
-	var ref_manager = null;
-	
-	if (!(ref_manager = RefreshManager.Initialize(e))){
-		
-	}
+	var root_elem='content';
 
 	//если это ссылка, берем из адреса
 	//если это кнопка, берем из formAction-атрибута
+	
 	var target = 
 		e.target.href.substr(location.origin.length)
 		|| e.target.formAction;					
@@ -369,21 +16,34 @@ function fragment_refresh1(e){
 	
 	//если цель не найдена, выкидываем ошибку
 	if (!target) {
+								
 		alert('fragment_refresh без цели');
 		throw new Error('fragment_refresh без цели');
 	};
 	
 	//для <ie10 осуществляем прямой переход
-	if (!window.atob)document.location.href = target;
-	else 
-		//если нет, кастомизируем:
-		e.preventDefault();
+	if (!window.atob){   //<ie10
+	
+		document.location.href = target;
+	}
+							
+	
+	e.preventDefault();
 	
 	
 	
 	/*определяем, есть ли те или иные элементы на странице, чтобы собщить серверу, какой из
 	шаблонов рендерить и возвращать*/
-
+	
+	/*теоретически это можно получать на сервере 
+	из адреса страницы, но тогда по сути придется проверять весь список адресов
+	(регулярк) на соответствие, после нахождения
+	необходимо распарсить шаблон и найти, есть ли там этот элемент. Проще найти тут:
+	*/
+	
+	
+	//content			
+	
 	//main(username|age|city),section
 	var unique_templates = 
 		e.target.dataset['_refresh']
