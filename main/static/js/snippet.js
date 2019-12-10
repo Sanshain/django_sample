1a27ad66c66be76e40f6ff73da8091ad25199abf
Sanshain <digital-mag@ya.ru>
2019-12-10 09:43:00 +0300

    fix errors until unwrapped level

--- main/static/js/snippet.js
+++ main/static/js/snippet.js
@@ -1,4 +1,14 @@
 
+function fragment_refresh(event){
+
+	var rm = null;
+	if (rm = RefreshManager.Initialize(event)){
+		
+		rm.Commit();
+	}
+
+}	
+
 /*!
 
 	e - expected event object
@@ -38,18 +48,8 @@ RefreshManager.Initialize = function(e){
 
 function RefreshManager(e, root_elem){
 	
-	var self = this;
-	var root_elem=root_elem || 'content';   	//obsolete
 	
-	var unique_templates = 
-		e.target.dataset['_refresh']
-			.replace(/[\s]+/,'')
-			.split(',');	
-	
-	this.aim_blocks = [];			// id блоков для запроса 
-	
-	
-	var get_boxes = function(block_name){
+	this.get_boxes = function(block_name){
 		
 		
 		var details = block_name.split(">");
@@ -108,182 +108,7 @@ function RefreshManager(e, root_elem){
 		return _boxes.length ? _boxes : _box;
 	}
 	
-						
-	function _animate(elem, visible){
-		if (!visible){ // скрываем
-		
-			//можно засунуть в класс
-			elem.style.transition='0.5s ease-out';
-			elem.style.opacity = 0;			
-			elem.style.transform = 'scale(0.9,0.9)';								
-			var _content = search_fixed(elem);
 
-			if (!_content) return;
-			else 
-				_content.style.top = '0';
-			
-		}
-		else{
-		
-			var temp_top = null;
-		
-			//тут написать спец ф-ю, которая ищет элементы с fixed до первого дерева с дочерними элементами больше 1			
-			
-			var _content = null;
-			
-			if (elem.id == 'main' || elem.id == 'content'){
-				//var _content = elem.children[0];
-				var _content = search_fixed(elem);
-				
-				if (_content){
-//!
-					temp_top = window.getComputedStyle(_content).top;		//либо 
-					//temp_top = _content.offsetTop;
-					_content.style.top = '0';//*/
-				}
-				
-			}
-
-			//показывает информацию через 1 сек
-			setTimeout(function(){
-				//возврат в top после анимации, чтобы не скроллился
-				elem.style.transition = 'none';			
-				elem.style.transform = 'none';
-								
-				if (temp_top) _content.style.top = temp_top;
-				
-				setTimeout(function(){
-					elem.style.transition = '0.5s';
-				}, 20);
-				
-			},1000);								
-
-			elem.style.opacity =1;
-//!
-			if (!_content) return; //<-скорее всего это убрать
-			
-			elem.style.transform='scale(1,1)';			
-
-		}
-	}							
-	
-	
-	/*!  регулярует вызов функции анимации...
-	
-		\brief регулярует вызов функции анимации для каждого..
-		блока и подблока после получения данных
-		
-		@param deep - глубина ожидания
-		@param box - блок, для которого ожидается анимация
-		
-	*/
-	function _await__animate(deep, box){
-	
-		console.log(deep + ' - waiting for ' + box.id);
-		
-		
-		if (!deep){ //animation отсутствия интернета
-			
-			alert('нет соединения с сервером');
-			
-			return false;
-		}										
-	
-		setTimeout(function(){
-			
-			if (responsed_content){
-
-				render_page(
-					responsed_content.pop(), 
-					responsed_content.pop()
-				);	
-				
-				//происходит анимация
-				setTimeout(function(){ //box.style.opacity =1;
-					
-					
-					//box.className = 'block';
-					_animate(box, true);
-				}, 40);
-			}
-			else {
-				
-				_await__animate(--deep, box);
-			}			
-
-		},700);								
-	}	
-	
-	
-	var package_animate = function(block_name){
-		
-		var responsed_content = null;
-		
-		var _boxes = get_boxes(block_name);
-		
-		if (_boxes.length){
-			
-			for (var k=0;k<_boxes.length;k++){
-				
-				_animate(_boxes[k], false);
-				
-				_await__animate(3, _boxes[k]);
-			}
-		}
-		else{
-			_animate(_boxes, false);
-			
-			_await__animate(3, _boxes);
-						
-			self.aim_blocks.push(_boxes.id);			
-		}
-		
-	}
-	
-	for (var key in unique_templates){
-		
-		package_animate(unique_templates[key]);
-	}
-		
-	var nec_blocks = requested_blocks_by_require();
-
-	//var blocks = aim_blocks.concat(nec_blocks);
-	
-	
-	
-	this.Commit = function(){
-		var box_onload = function (resp, set_url)
-		{
-			responsed_content = [set_url,resp];			
-		}		
-			
-		var ajax = new Ajax(this.target, box_onload);
-		ajax.onfail = function(){
-			//здесь может быть относительно 
-			// навязчивое сообщение о том, что 
-			//ваш браузер не поддерживает 
-			//автоматические переходы
-			alert('ваш браузер не смог осуществить частичное обновление контента страницы.'+
-			' Нажмите ок, чтобы перейти напрямую');
-			document.location.href = this.target;
-		};
-		
-		var args = self.target.match(/\d+/g);//аргументы
-		
-		var q = [args, nec_blocks, self.aim_blocks];
-		
-		ajax.submit_json(q);		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	function requested_blocks_by_require(){	
 	
 		var requared_blocks = 
@@ -341,6 +166,198 @@ function RefreshManager(e, root_elem){
 		return r_blocks;
 		
 	}
+		
+						
+	function _animate(elem, visible){
+		if (!visible){ // скрываем
+		
+			//можно засунуть в класс
+			elem.style.transition='0.5s ease-out';
+			elem.style.opacity = 0;			
+			elem.style.transform = 'scale(0.9,0.9)';								
+			var _content = search_fixed(elem);
+
+			if (!_content) return;
+			else 
+				_content.style.top = '0';
+			
+		}
+		else{			//показываем:
+		
+			var temp_top = null;
+		
+			//тут написать спец ф-ю, которая ищет элементы с fixed до первого дерева с дочерними элементами больше 1			
+			
+			var _content = null;
+			
+			if (elem.id == 'main' || elem.id == 'content'){
+				//var _content = elem.children[0];
+				var _content = search_fixed(elem);
+				
+				if (_content){
+//!
+					temp_top = window.getComputedStyle(_content).top;		//либо 
+					//temp_top = _content.offsetTop;
+					_content.style.top = '0';//*/
+				}
+				
+			}
+
+			//показывает информацию через 1 сек
+			
+			//elem.style.transform = 'scale(1,1)';
+			
+			setTimeout(function(){
+				//возврат в top после анимации, чтобы не скроллился
+				elem.style.transition = 'none';			
+				elem.style.transform = 'none';
+				
+				
+				if (temp_top) _content.style.top = temp_top;
+				
+				setTimeout(function(){
+					elem.style.transition = '0.5s';
+				}, 40);//*/
+				
+			},1000);	//*/							
+
+			elem.style.opacity =1;
+//!
+			//if (!_content) return; //<-скорее всего это убрать
+			
+			elem.style.transform='scale(1,1)';			
+
+		}
+	}							
+	
+	
+	/*!  регулярует вызов функции анимации...
+	
+		\brief регулярует вызов функции анимации для каждого..
+		блока и подблока после получения данных
+		
+		@param deep - глубина ожидания
+		@param box - блок, для которого ожидается анимация
+		
+	*/
+	function _await__animate(deep, box){
+	
+		console.log(deep + ' - waiting for ' + box.id);
+		
+		
+		if (!deep){ //animation отсутствия интернета
+			
+			alert('нет соединения с сервером');
+			
+			return false;
+		}										
+	
+		setTimeout(function(){
+			
+			if (responsed_content){
+
+				render_page(
+					responsed_content.pop(), 
+					responsed_content.pop()
+				);	
+				
+				//происходит анимация
+				setTimeout(function(){ //box.style.opacity =1;
+					
+					
+					//box.className = 'block';
+					_animate(box, true);
+				}, 40);
+			}
+			else {
+				
+				_await__animate(--deep, box);
+			}			
+
+		},700);								
+	}	
+	
+	
+	this.package_animate = function(block_name){
+		
+		var responsed_content = null;
+		
+		var _boxes = this.get_boxes(block_name);
+		
+		if (_boxes.length){
+			
+			for (var k=0;k<_boxes.length;k++){
+				
+				_animate(_boxes[k], false);
+				
+				_await__animate(3, _boxes[k]);
+			}
+		}
+		else{
+			_animate(_boxes, false);
+			
+			_await__animate(3, _boxes);
+						
+			self.aim_blocks.push(_boxes.id);			
+		}
+		
+	}
+	
+
+	this.Commit = function(){
+		var box_onload = function (resp, set_url) //
+		{
+			responsed_content = [set_url,resp];			
+		}		
+			
+		var ajax = new Ajax(this.target, box_onload);
+		ajax.onfail = function(){
+			//здесь может быть относительно 
+			// навязчивое сообщение о том, что 
+			//ваш браузер не поддерживает 
+			//автоматические переходы
+			alert('ваш браузер не смог осуществить частичное обновление контента страницы.'+
+			' Нажмите ок, чтобы перейти напрямую');
+			document.location.href = this.target;
+		};
+		
+		var args = self.target.match(/\d+/g);//аргументы
+		
+		var q = [args, nec_blocks, self.aim_blocks];
+		
+		ajax.submit_json(q);		
+	}
+	
+	
+
+
+
+				/* блок инициализации: */
+
+	var self = this;
+	var root_elem=root_elem || 'content';   	//obsolete
+	
+	var unique_templates = e.target.dataset['_refresh']
+			.replace(/[\s]+/,'')
+			.split(',');	
+	
+	
+	this.aim_blocks = [];			// id блоков для запроса 
+	
+	
+	
+					/* блок выполнения: */
+	
+	for (var key in unique_templates){
+		
+		this.package_animate(unique_templates[key]);
+	}
+		
+	var nec_blocks = requested_blocks_by_require();
+
+	//var blocks = aim_blocks.concat(nec_blocks);
+	
+		
 	
 }
 
