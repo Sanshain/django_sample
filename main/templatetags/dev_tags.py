3bee3e797d22cb50115cb3407f4d8c6af8eeca0d
DigmaBook <digital-mag@ya.ru>
2020-02-13 13:38:24 +0300

    add tags `frag` and `unit`

--- main/templatetags/dev_tags.py
+++ main/templatetags/dev_tags.py
@@ -12,3 +12,20 @@ def dev_include(parser, token):
     if not settings.HAML_COMPILE: token.contents = token.contents.replace('.haml','.html')
 
     return do_include(parser, token)
+
+
+@register.tag('frag')
+def frag_include(parser, token):
+
+    token = 'fragments/{}.{}'.format(token, settings.TEMPLATE_EXTENSION)
+
+    return do_include(parser, token)
+
+
+@register.tag('unit')
+def frag_include(parser, token):
+
+    token = 'components/{}.{}'.format(token, settings.TEMPLATE_EXTENSION)
+
+    return do_include(parser, token)
+
